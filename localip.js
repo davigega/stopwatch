c534243510ed92b1c4271cedb73215f70d84b8d3
davigega <davigega@gmail.com>
2019-02-02 12:25:42 +0100

    log interfaces

--- localip.js
+++ localip.js
@@ -7,7 +7,7 @@ var ifaces = os.networkInterfaces();
 
 Object.keys(ifaces).forEach(function (ifname) {
   var alias = 0;
-
+  console.log(ifaces);
   ifaces[ifname].forEach(function (iface) {
     if ('IPv4' !== iface.family || iface.internal !== false) {
       // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
